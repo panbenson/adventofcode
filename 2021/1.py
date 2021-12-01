e9fa54518008392e59f529e45fea47b1509998da
Benson Pan <benson.pan13@gmail.com>
2021-12-01 00:22:02 -0500

    add some comments

--- 2021/1.py
+++ 2021/1.py
@@ -1,3 +1,4 @@
+# https://adventofcode.com/2021/day/1
 def day_one():
     with open('1.in', 'r') as reader:
         prev = 99999
@@ -16,6 +17,7 @@ def day_one_part_two():
         increasing = 0
         nums = [int(line) for line in reader]
 
+        # exclude the last 2 because they don't form a window of 3
         for i in range(len(nums) - 2):
             num = sum(nums[i:i+3])
             # print(num)
