a99a87ab346986588deff5be4eb9464d39f1cf35
Benson Pan <benson.pan13@gmail.com>
2021-12-01 00:20:05 -0500

    day 1 solutions

--- 2021/1.py
+++ 2021/1.py
@@ -1,7 +1,30 @@
 def day_one():
     with open('1.in', 'r') as reader:
+        prev = 99999
+        increasing = 0
         for line in reader:
-            print(line)
+            num = int(line)
+            if prev < num:
+                increasing += 1
+            prev = num
+        print(increasing)
+
+
+def day_one_part_two():
+    with open('1.in', 'r') as reader:
+        prev = 99999
+        increasing = 0
+        nums = [int(line) for line in reader]
+
+        for i in range(len(nums) - 2):
+            num = sum(nums[i:i+3])
+            # print(num)
+            # only consider increase NOT no change :(
+            if prev < num:
+                increasing += 1
+            prev = num
+        print(increasing)
 
 
 day_one()
+day_one_part_two()
